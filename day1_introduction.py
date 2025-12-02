d98f1821365525d5d1f6545497b99317b7946bb7
OliverRatcliff1 <oliverratcliff01@gmail.com>
2025-12-02 13:13:29 +0000

    committed

---
+++ day1_introduction.py
@@ -0,0 +1,41 @@
+# Day 1 - Introduction
+
+print(2+3) # addition
+print(3-1) # subtraction
+print(2*3) # multiplication
+print(3/2) # divison
+print(3**2) # exponential
+print(3%2) # Modulus
+print(3//2) # floor divison 
+
+# Checking data types
+print(type(10)) # Integer
+print(type(3.14)) # Float 
+print(type(1+3j)) # Complex Number
+print(type('Oliver')) # String
+print(type([1,2,3])) # List
+print(type({'name':'Oliver'})) # Dictionary
+print(type({9.8,3.14,2.7})) # Set 
+print(type((9.8,3.14,2.7))) # Tuple
+
+# Exercises 
+print('Oliver') # Name
+print('Ratcliff') # Family Name 
+print('United Kingdom') # Country
+print('I am enjoying learning Python') # Statement
+# ---
+print(type(10))
+print(type(9.8))
+print(type(4-4j))
+print(type('Oliver'))
+print(type(([1,2,3,4,5])))
+print(type({'Oliver', 'Python', 'United Kingdom'}))
+print(type({'name' :'Oliver'}))
+print(type((5.5,6.5,7.5,8.5)))
+# ---
+print(type(10))
+print(type(3.5))
+print(type(4j))
+print(type('Oliver Ratcliff'))
+print(type(['rain','thunder','lightning']))
+print(type(('rain','thunder','lightning')))
