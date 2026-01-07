661551b87f79392b46d173a8a2f21b84761e4e9a
OliverRatcliff1 <oliverratcliff01@gmail.com>
2026-01-07 11:59:42 +0000

    DAy 4 - Strings

--- day4_strings.py
+++ day4_strings.py
@@ -54,3 +54,70 @@ print(formatted_string)
 
 a=4
 b=3
+print('{}+{}={}'.format(a,b,a+b))
+print('{} + {} = {}'.format(a, b, a + b))
+print('{} - {} = {}'.format(a, b, a - b))
+print('{} * {} = {}'.format(a, b, a * b))
+print('{} / {} = {:.2f}'.format(a, b, a / b)) # limits it to two digits after decimal
+print('{} % {} = {}'.format(a, b, a % b))
+print('{} // {} = {}'.format(a, b, a // b))
+print('{} ** {} = {}'.format(a, b, a ** b))
+
+# Strings and Numbers 
+radius=10
+pi=3.14
+area=pi*radius**2
+formatted_string='The area of a circle with a radius of {} is {:.2f}'.format(radius, area)
+print(formatted_string)
+
+# String Interpolation / f-strings 
+c=4
+d=3
+print(f'{a} + {b} = {a+b}')
+print(f'{a} / {b} = {a / b:.2f}')
+
+# Python strings as sequences of characters (numbers start from 0)
+language='Python'
+first_letter=language[0]
+print(first_letter)
+second_letter=language[1]
+print(second_letter)
+last_index=len(language)-1
+last_letter=language[last_index]
+print(last_letter)
+
+# Slicing Strings 
+language = 'Python'
+first_three = language[0:3] # starts at zero index and up to 3 but not include 3
+print(first_three) #Pyt
+last_three = language[3:6]
+print(last_three) # hon
+# Another way
+last_three = language[-3:]
+print(last_three)   # hon
+last_three = language[:3]
+print(last_three)   # hon
+# :3 = everything beofre index 3 / 3: everything after index 3 
+
+# Reversing String 
+greeting='Hello, World!'
+print(greeting[::-1])
+
+# Skipping Characters ( the 6 means stop before index 6 / the 2 takes every 2nd character
+language = 'Python'
+pto = language[0:6:2] #
+print(pto) # pto
+
+# String Methods 
+# Capitalize - first letter a capital
+challenge='thirty days of python'
+print(challenge.capitalize())
+# Count - returns occurances 
+hallenge = 'thirty days of python'
+print(challenge.count('y')) # 3
+print(challenge.count('y', 7, 14)) # 1
+print(challenge.count('th')) # 2
+#Endswith - checks if string ends with a specific ending
+challenge = 'thirty days of python'
+print(challenge.endswith('on'))   # True
+print(challenge.endswith('tion')) # False
